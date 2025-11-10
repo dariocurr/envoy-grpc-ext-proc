30f2bd850bf9e6f6683a86f81e7e655c98470ae2
dario curreri <dariocurr@gmail.com>
2025-11-10 18:38:03 +0100

    chore: add protoc-bin-vendored dependency and configure build script

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    println!("cargo:rustc-env=PROTOC={}", protoc.display());
     tonic_prost_build::configure()
         .build_client(true)
         .build_server(true)
